1416c75ea8e323795733c8320e17280e48995d62
shaahidbasic <shaahidbasic101@gmail.com>
2020-09-23 01:06:48 +0530

    introduction to the homework

---
+++ src/multiple_questions.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class multiple_questions {
+
+    public static void main(String[] args) {
+
+        //question 1
+//        int start = 1;
+//        while (start < 11){
+//
+//            System.out.println(start);
+//            start += 1;
+//        }
+
+        //question 2
+//        int sum = 0;
+//        for(int i = 0;i <= 10;i++){
+//            sum += i;
+//        }
+//        System.out.println("Sum: " + sum);
+
+        //question 3
+//        Scanner sc = new Scanner(System.in);
+////        System.out.println("enter a positive number: ");
+////        int num = sc.nextInt();
+////
+////        for(int i=1; i<=10; i++)
+////        {
+////            System.out.println(num +" x " + i + " = " + (num*i) );
+////        }
+
+        //question 4
+//        Scanner console = new Scanner(System.in);
+//        int num; // To hold number
+//        int fact = 1; // To hold factorial
+//
+//        System.out.print("Enter any positive integer: ");
+//        num = console.nextInt();
+//
+//        for(int i=1; i<=num; i++)
+//        {
+//            fact *= i;
+//        }
+//
+//        System.out.println("Factorial: "+ fact);
+
+        //question 5
+//        Scanner console = new Scanner(System.in);
+//
+//        int base;
+//        int power;
+//        int result = 1;
+//
+//        System.out.print("Enter the base number ");
+//        base = console.nextInt();
+//
+//        System.out.print("Enter the power ");
+//        power = console.nextInt();
+//
+//        for(int i = 1; i <= power; i++)
+//        {
+//            result *= base;
+//        }
+//
+//        System.out.println("Result: "+ result);
+
+        //question 6
+
+        //question 7
+
+        //question 8
+
+        //question 9
+
+        //question 10
+
+        //question 11
+
+        //question 12
+
+        //question 13
+
+        //question 14
+
+        //question 15
+
+        //question 16
+
+        //question 17
+
+        //question 18
+
+        //question 19
+
+        //question 20
+
+    }
+}
